d3f920c249b58e3c30f4dad19296ebdefb169432
Primelle <pri_melle@hotmail.com>
2020-09-21 23:23:34 +0200

    opdracht 7

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+var rotationSpeed = 0.05;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
@@ -13,3 +13,6 @@ console.log(myOtherBox.object3D.rotation.x);
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
+
+
+
